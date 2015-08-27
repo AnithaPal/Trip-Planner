e7dc601bb001c8b660aae08a53e67b7a253d9217
AnithaPal <anitha.aravindvel@gmail.com>
2015-08-27 12:37:39 -0400

    trip view design updated

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -1,13 +1,60 @@
 class TripsController < ApplicationController
   def index
+    @trips = Trip.all
   end
 
   def show
+    @trip = Trip.find(params[:id])
   end
 
   def new
+    @trip = Trip.new
+    @trip.user = current_user
+  end
+
+  def create
+    @trip = Trip.new(trip_params)
+    @trip.user = current_user
+
+    if @trip.save
+      flash[:notice] = "Trip was created successfully"
+       redirect_to @trip
+    else
+      flash[:error] = "Sorry, There was some an error when creating your trip. Please try again."
+      render :new
+    end
   end
 
   def edit
+    @trip = Trip.find(params[:id])
+  end
+
+  def update
+    @trip = Trip.find(params[:id])
+
+    if @trip.update_attributes(trip_params)
+      flash[:notice] = "Trip was updated successfully"
+      redirect_to @trip
+    else
+      flash[:error] = "Sorry, There was some an error when updating your trip. Please try again."
+      render :edit
+    end
+  end
+
+  def destroy
+    @trip = Trip.find(params[:id])
+    if @trip.destroy
+      flash[:notice] = "Trip was successfully deleted"
+      redirect_to trips_path
+    else
+      flash[:error] = "There was an error deleting your trip. Please try again."
+        render :show
+    end
+  end
+
+  private
+  def trip_params
+    params.require(:trip).permit(:name)
+
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150825021340) do
+ActiveRecord::Schema.define(version: 20150827153615) do
+
+  create_table "trips", force: :cascade do |t|
+    t.string   "name"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "trips", ["user_id"], name: "index_trips_on_user_id"
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
