e74f54ec53fa973dfeb4e4f368bb021fc335b205
AnithaPal <anitha.aravindvel@gmail.com>
2015-08-27 09:36:25 -0400

    signin through google configured

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
 
-
-  # devise_for :users
   devise_for :users, :controllers => { omniauth_callbacks: 'omniauth_callbacks' }
   get 'about' => 'welcome#about'
   root to: 'welcome#index'
