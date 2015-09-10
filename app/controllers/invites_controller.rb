31a24116421277f66bfe6e70c00694d984ff3dd4
AnithaPal <anitha.aravindvel@gmail.com>
2015-09-10 16:38:23 -0400

    status added to invite

--- app/controllers/invites_controller.rb
+++ app/controllers/invites_controller.rb
@@ -4,26 +4,25 @@ class InvitesController < ApplicationController
     @user = User.find_by(email: params[:invite][:email])
     @trip = Trip.find(params[:invite][:trip_id])
 
-    if @trip.user == @user || @trip.users.include?(@user)
-      flash[:error] = "#{@user.email} is already invited to this #{@trip.name}"
-    elsif @user.present?
-      @tripper = Tripper.create(user: @user, trip: @trip)
-      @sender = current_user
-      # Send email telling them they are invited to trip
-      InviteMailer.invite_existing_user(@user, @trip, @sender).deliver
-      flash[:notice] = "An email invite was sent to #{@user.email} and added to the trip!"
-    else
-      @invite = Invite.new(invite_params)
-      @invite.sender_id = current_user.id
+    @invite = Invite.new(invite_params)
+    @invite.sender_id = current_user.id
 
-      if @invite.save
+    if @invite.save
+      if @trip.user == @user || @trip.users.include?(@user)
+        flash[:error] = "#{@user.email} is already invited to this #{@trip.name}"
+      elsif @user.present?
+        @tripper = Tripper.create(user: @user, trip: @trip)
+        @sender = current_user
+        # Send email telling them they are invited to trip
+        InviteMailer.invite_existing_user(@user, @trip, @sender).deliver
+        flash[:notice] = "An email invite was sent to #{@user.email} and added to the trip!"
+      else
         InviteMailer.invite_new_user(@invite, new_user_registration_url(:invite_token => @invite.token)).deliver #send the invite data to our mailer to deliver the email
         flash[:notice] = "Trip invitaion has successfully sent to #{@invite.email}."
-      else
-        flash[:error] = "Sorry, there was some error in sedning an invite. please try again"
       end
+    else
+        flash[:error] = "Sorry, there was some error in sedning an invite. please try again"
     end
-
     redirect_to @trip
   end
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150910172048) do
+ActiveRecord::Schema.define(version: 20150910184254) do
 
   create_table "invites", force: :cascade do |t|
     t.string   "email"
@@ -19,8 +19,9 @@ ActiveRecord::Schema.define(version: 20150910172048) do
     t.integer  "sender_id"
     t.integer  "recipient_id"
     t.string   "token"
-    t.datetime "created_at",   null: false
-    t.datetime "updated_at",   null: false
+    t.datetime "created_at",                   null: false
+    t.datetime "updated_at",                   null: false
+    t.boolean  "accepted",     default: false
   end
 
   add_index "invites", ["trip_id"], name: "index_invites_on_trip_id"
