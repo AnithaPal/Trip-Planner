706a86cb97863d577c9dd0b4a8bed500287df834
AnithaPal <anitha.aravindvel@gmail.com>
2015-12-04 10:08:58 -0500

    deliver method updated in invite controller

--- app/controllers/invites_controller.rb
+++ app/controllers/invites_controller.rb
@@ -12,10 +12,10 @@ class InvitesController < ApplicationController
     elsif @invite.save
       if @user.present?
         @invite.recipient.id = @user.id
-        InviteMailer.invite_existing_user(@invite, @trip).deliver
+        InviteMailer.invite_existing_user(@invite, @trip).deliver_now
         flash[:notice] = "An email invite was sent to #{@user.email}"
       else
-        InviteMailer.invite_new_user(@invite, @trip).deliver
+        InviteMailer.invite_new_user(@invite, @trip).deliver_now
         flash[:notice] = "Trip invitaion has successfully sent to #{@invite.email}."
       end
     else
