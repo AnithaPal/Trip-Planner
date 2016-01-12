659a116cad456483dbccff00925ae3cbb1b3d4b8
AnithaPal <anitha.aravindvel@gmail.com>
2016-01-12 16:24:21 -0500

    email configuration change for production

--- config/initializers/mail.rb
+++ config/initializers/mail.rb
@@ -1,9 +1,9 @@
 ActionMailer::Base.smtp_settings = {
-  port:              587, 
+  port:              587,
   address:           'smtp.mailgun.org',
   user_name:         ENV['MAILGUN_SMTP_LOGIN'],
   password:          ENV['MAILGUN_SMTP_PASSWORD'],
-  domain:            'appurlhere.com',
+  domain:            'trp-plan.heroku.com',
   authentication:    :plain,
   content_type:      'text/html'
 }
@@ -12,13 +12,13 @@ ActionMailer::Base.delivery_method = :smtp
 # Makes debugging *way* easier.
 ActionMailer::Base.raise_delivery_errors = true
 
-class DevelopmentMailInterceptor
-  def self.delivering_email(message)
-    message.to = "codetrain15@gmail.com"
-    message.cc = nil
-    message.bcc = nil
-  end
-end
+# class DevelopmentMailInterceptor
+#   def self.delivering_email(message)
+#     message.to = "codetrain15@gmail.com"
+#     message.cc = nil
+#     message.bcc = nil
+#   end
+# end
 
 if Rails.env.development?
   ActionMailer::Base.register_interceptor(DevelopmentMailInterceptor)
